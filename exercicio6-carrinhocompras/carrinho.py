0c81f1ed90daad4425e0f014a9e037ae5f9fd6cc
gabrielmartielo <devgabrielmartielo@gmail.com>
2026-09-04 11:26:24 -0300

    feat: add calcular_total_carrinho function and corresponding tests

--- exercicio6-carrinhocompras/carrinho.py
+++ exercicio6-carrinhocompras/carrinho.py
@@ -5,3 +5,12 @@ def calcular_total_item(preco, quantidade):
         raise ValueError("Preço e Quantidade devem ser maiores que zero.")
     return totalItem
 
+def calcular_total_carrinho(carrinho):
+    totalCarrinho = 0
+    for i in carrinho:
+        valorItem = i["preco"] * i["quantidade"]
+        totalCarrinho += valorItem
+    if totalCarrinho >= 200:
+        valorDesconto = totalCarrinho * 10 / 100
+        totalCarrinho -= valorDesconto
+    return totalCarrinho
--- exercicio6-carrinhocompras/tests/test_carrinho.py
+++ exercicio6-carrinhocompras/tests/test_carrinho.py
@@ -1,6 +1,5 @@
-from carrinho import calcular_total_item
+from carrinho import calcular_total_item, calcular_total_carrinho
 import pytest
-
 
 def test_calcular_total_item():
     assert calcular_total_item(50, 2) == 100
@@ -8,3 +7,20 @@ def test_calcular_total_item():
         calcular_total_item(50, 0)
     with pytest.raises(ValueError, match=r"Preço e Quantidade devem ser maiores que zero."):
         calcular_total_item(-10, 2)
+
+def test_calcular_total_carrinho():
+    carrinho_exemplo = [
+        {"preco": 50, "quantidade": 2},
+        {"preco": 30, "quantidade": 1}
+    ]
+    
+    assert calcular_total_carrinho(carrinho_exemplo) == 130
+
+
+def test_calcular_total_carrinho_com_desconto():
+    carrinho_exemplo = [
+        {"preco": 100, "quantidade": 1},
+        {"preco": 100, "quantidade": 1},
+        {"preco": 100, "quantidade": 1}
+    ]
+    assert calcular_total_carrinho(carrinho_exemplo) == 270
